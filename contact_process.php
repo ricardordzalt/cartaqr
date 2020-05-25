0516c853503e02ffe08e5bb4b9f4750db875de29
danielsan2020 <jdanielsancheztaza@gmail.com>
2020-05-25 09:40:38 -0500

    tucartaqr

---
+++ contact_process.php
@@ -0,0 +1,38 @@
+<?php
+
+    $to = "hola@tucartaqr.mx";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "Tienes un mensaje de Tu CartaQR";
+
+    $link = 'https://www.tucartaqr.mx';
+
+	$body = "<!DOCTYPE html><html lang='es'><head><meta charset='UTF-8'><title>Tu CartaQR</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+	$send = mail($to, $subject, $body, $headers);
+	
+	header('location:./contacto.html');
+
+?>
