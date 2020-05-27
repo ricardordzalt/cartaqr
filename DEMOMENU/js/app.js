ba926b614e456a38bccae9e43cdf5837a0684d52
Jose Urbina <j.urbina.0183@Gmail.com>
2020-05-27 09:34:16 -0400

    arreglos menores

--- DEMOMENU/js/app.js
+++ DEMOMENU/js/app.js
@@ -1,23 +1,28 @@
 
 let description = document.querySelectorAll('.conatiner-description')
 
-let platillo = document.querySelectorAll('.food-link')
+let platillo = document.querySelectorAll('.single-menu-item')
 
 
 platillo.forEach(item => {
-    item.addEventListener('click', (e) => {
-        
-        console.log(item.parentElement.previousSibling.previousElementSibling.classList.toggle('desactive'))
-        e.preventDefault()
+    item.addEventListener('click', () => {
+        const elemet = item.parentElement.previousSibling.previousElementSibling
+        if(elemet.classList.contains('conatiner-description')) {
+            console.log('funciona!!')
+            console.log(elemet.classList.toggle('desactive'))
+        }
     })
 })
 
 description.forEach(item => {
-    
         let salir = item.firstChild.nextSibling.firstChild.nextSibling.firstChild.nextSibling
-        
+        let elemet = salir.parentNode.parentNode.parentNode
         salir.addEventListener('click', () => {
-            salir.parentNode.parentNode.parentNode.classList.toggle('desactive')
+            if(elemet.classList.contains('conatiner-description')) {
+                console.log('hola')
+                elemet.classList.toggle('desactive')
+            }
+           
         })
     
 })
