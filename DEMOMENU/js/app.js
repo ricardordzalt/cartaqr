e397b50ec69a21689a5d434181937f329cbee7c8
Jose Urbina <j.urbina.0183@Gmail.com>
2020-05-26 20:28:45 -0400

    modal descripción

---
+++ DEMOMENU/js/app.js
@@ -0,0 +1,23 @@
+
+let description = document.querySelectorAll('.conatiner-description')
+
+let platillo = document.querySelectorAll('.food-link')
+
+
+platillo.forEach(item => {
+    item.addEventListener('click', (e) => {
+        
+        console.log(item.parentElement.previousSibling.previousElementSibling.classList.toggle('desactive'))
+        e.preventDefault()
+    })
+})
+
+description.forEach(item => {
+    item.addEventListener('click', () => {
+        let salir = item.firstChild.nextSibling.firstChild.nextSibling.firstChild.nextSibling
+        
+        salir.addEventListener('click', () => {
+            salir.parentNode.parentNode.parentNode.classList.toggle('desactive')
+        })
+    })
+})
